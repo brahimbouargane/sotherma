9dc543240744b43e6b0ce6dea0b6e035b77ce47f
brahimbouargane <brahim.bouargane2000@gmail.com>
2025-05-13 12:13:49 +0100

    add last changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,12 @@
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react-swc";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
@@ -5,4 +14,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0", // This allows connections from all IPs
+    port: 8080, // Match the port DigitalOcean is checking
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: 8080,
+  },
 });
